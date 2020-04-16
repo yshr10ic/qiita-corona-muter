4e82b9991fc05cab1e4b31fa8d98605ff1e307b8
yshr10ic <yshr10ic@gmail.com>
2020-04-16 20:51:45 +0900

    urlによる条件分岐を追加

--- js/content.js
+++ js/content.js
@@ -1,12 +1,30 @@
+var url = location.href;
+url = url.replace($(location).attr('search'), '');
+
+switch(url) {
+    // トレンド
+    case 'https://qiita.com/':
+        $(function() {
+            muteTrend();
+        });
+        break;
+    default:
+        console.log(url);
+        break;
+}
+
 /**
  * トレンドの記事一覧で、タイトルにコロナが入っている場合にタイトルを非表示する
  */
-$(".tr-Item").each(function(index, element) {
-    if (includeCorona($(element).children("div").children("a").text())) {
-        $(element).hide();
-        // $(element).children("div").css("color", "red");
-    }
-});
+function muteTrend() {
+    console.log("muteTimeline is called");
+    $(".tr-Item").each(function(index, element) {
+        if (includeCorona($(element).children("div").children("a").text())) {
+            $(element).hide();
+            // $(element).children("div").css("color", "red");
+        }
+    });
+}
 
 function includeCorona(text) {
     let regex = /コロナ/g;
