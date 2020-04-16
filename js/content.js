476d9dc8f0d567bed762dd692550392e363c1387
yshr10ic <yshr10ic@gmail.com>
2020-04-16 17:33:40 +0900

    タイムライン上でタイトルにコロナが入っている記事を非表示にする

--- js/content.js
+++ js/content.js
@@ -1,1 +1,14 @@
-alert('Help!');
+/**
+ * トレンドの記事一覧で、タイトルにコロナが入っている場合にタイトルを非表示する
+ */
+$(".tr-Item").each(function(index, element) {
+    if (includeCorona($(element).children("div").children("a").text())) {
+        $(element).hide();
+        // $(element).children("div").css("color", "red");
+    }
+});
+
+function includeCorona(text) {
+    let regex = /コロナ/g;
+    return text.search(regex) != -1;
+}
