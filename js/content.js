1e76b7a66042541a6db31b63edef133d66ff16e8
yshr10ic <yshr10ic@gmail.com>
2020-04-18 10:12:16 +0900

    タイムライン対応

--- js/content.js
+++ js/content.js
@@ -1,5 +1,6 @@
 var url = location.href;
-url = url.replace($(location).attr('search'), '');
+
+console.log(url);
 
 switch(url) {
     // トレンド
@@ -7,6 +8,12 @@ switch(url) {
         $(function() {
             muteTrend();
         });
+        break;
+    // タイムライン
+    case 'https://qiita.com/timeline':
+        setInterval(function() {
+            muteTimeline('tl');
+        }, 500);
         break;
     // タグフィード
     case 'https://qiita.com/tag-feed':
@@ -29,21 +36,31 @@ switch(url) {
  * トレンドの記事一覧で、タイトルにコロナが入っている場合にタイトルを非表示する
  */
 function muteTrend() {
-    console.log("muteTimeline is called");
-    $(".tr-Item").each(function(index, element) {
-        if (includeCorona($(element).children("div").children("a").text())) {
-            $(element).hide();
-            // $(element).children("div").css("color", "red");
+    console.log('muteTrend is called');
+    $('.tr-Item').each(function(index, element) {
+        if (includeCorona($(element).children('div').children('a').text())) {
+            // $(element).hide();
+            $(element).children('div').css('color', 'red');
+        }
+    });
+}
+
+function muteTimeline(prefix) {
+    console.log('mute ' + prefix + ' is called');
+    $('.' + prefix + '-Item').each(function(index, element) {
+        if (includeCorona($(element).children('div').children('.' + prefix + '-ItemContent').children('div').children('div').children('a').text())) {
+            // $(element).hide();
+            $(element).children('div').children('.' + prefix + '-ItemContent').children('div').children('div').css('color', 'red');
         }
     });
 }
 
 function mute(prefix) {
-    console.log("mute " + prefix + " is called");
-    $("." + prefix + "-Item").each(function(index, element) {
-        if (includeCorona($(element).children("div").children("." + prefix + "-ItemContent").children("div").children("a").text())) {
-            $(element).hide();
-            // $(element).children("div").children("." + prefix + "-ItemContent").children("div").css("color", "red");
+    console.log('mute ' + prefix + ' is called');
+    $('.' + prefix + '-Item').each(function(index, element) {
+        if (includeCorona($(element).children('div').children('.' + prefix + '-ItemContent').children('div').children('a').text())) {
+            // $(element).hide();
+            $(element).children('div').children('.' + prefix + '-ItemContent').children('div').css('color', 'red');
         }
     });
 }
